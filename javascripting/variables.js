25a121016f73aa95f53e72c1edf2195113bb14e8
smeet96 <smeetsherki@gmail.com>
2025-09-02 17:06:30 +0530

    added more solutions

---
+++ javascripting/variables.js
@@ -0,0 +1,13 @@
+// # NOTE  
+   
+//   A variable is declared using let and uses the equals sign to define the  
+//   value that it references. This is colloquially known as "Making a variable  
+//   equal a value".  
+   
+//  ## The challenge:  
+
+
+//   Then use console.log() to print the example variable to the console.
+
+let example = "some string"
+console.log(example)
